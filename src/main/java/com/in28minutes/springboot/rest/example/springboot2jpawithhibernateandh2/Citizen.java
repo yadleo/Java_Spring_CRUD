8cfeffa58f2f2b21049635d6bb69d42cd0e7d6b3
Daniel Lai <dlai813@gmail.com>
2019-03-18 16:57:00 -0400

    [In Progress] connecting with mysql

--- src/main/java/com/in28minutes/springboot/rest/example/springboot2jpawithhibernateandh2/Citizen.java
+++ src/main/java/com/in28minutes/springboot/rest/example/springboot2jpawithhibernateandh2/Citizen.java
@@ -1,16 +1,22 @@
 package com.in28minutes.springboot.rest.example.springboot2jpawithhibernateandh2;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table(name = "citizens")
 public class Citizen {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Allows auto increment from mysql side
     private Long id;
     private String name;
     private String passportNumber;
+
+    @Column(columnDefinition = "date")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date expiration;
 
     public Long getId() {
         return id;
@@ -34,5 +40,13 @@ public class Citizen {
 
     public void setPassportNumber(String passportNumber) {
         this.passportNumber = passportNumber;
+    }
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
     }
 }
